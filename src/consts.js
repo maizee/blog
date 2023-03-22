244fc8cb0f480038a2e4682f52eecd7b751d55a6
yuch <mungbeanlee@163.com>
2023-03-22 22:49:11 +0800

    教程学习

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,6 @@
-export const SITE_TITLE = `Air Blog`;
+export const SITE_TITLE = `Yuch`;
 export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_QQ = 1911428610
+export const SITE_EMAIL = 'mungbeanlee@163.com'
+export const SITE_NAME = 'liyucheng.com';
+export const SITE_URL = "https://liyucheng.com";
