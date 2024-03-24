9d7d0f366afed32a4ef1041d87b107154ef343ac
yuch <56346057+maizee@users.noreply.github.com>
2024-03-24 13:18:29 +0800

    增删文章

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-export const SITE_TITLE = `Yuch`;
+export const SITE_TITLE = `Yuch'Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
 export const SITE_QQ = 1911428610
 export const SITE_EMAIL = 'mungbeanlee@163.com'
