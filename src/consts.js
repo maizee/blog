5b95d73927245aef9afb765cfd25c25079d743e2
yuch <56346057+maizee@users.noreply.github.com>
2024-03-30 22:50:54 +0800

    修改底部

--- src/consts.js
+++ src/consts.js
@@ -2,5 +2,5 @@ export const SITE_TITLE = `Yuch'Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
 export const SITE_QQ = 1911428610
 export const SITE_EMAIL = 'mungbeanlee@163.com'
-export const SITE_NAME = 'liyucheng.com';
+export const SITE_NAME = 'liyucheng.xyz';
 export const SITE_URL = "https://blog-5mq.pages.dev/";
