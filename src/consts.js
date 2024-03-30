3cfbfa2b69729ce5b7135cf3f5c12d54b0c44ecc
yuch <56346057+maizee@users.noreply.github.com>
2024-03-30 23:44:49 +0800

    添加域名网站

--- src/consts.js
+++ src/consts.js
@@ -3,4 +3,4 @@ export const SITE_DESCRIPTION = 'Site Description';
 export const SITE_QQ = 1911428610
 export const SITE_EMAIL = 'mungbeanlee@163.com'
 export const SITE_NAME = 'liyucheng.xyz';
-export const SITE_URL = "https://blog-5mq.pages.dev/";
+export const SITE_URL = "https://liyucheng.xyz/";
