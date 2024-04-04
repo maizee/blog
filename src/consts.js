442d893a52a12acbaee7e767727176f6536eaa46
yuch <56346057+maizee@users.noreply.github.com>
2024-04-04 08:45:14 +0800

    改名

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Yuch'Blog`;
-export const SITE_DESCRIPTION = 'Site Description';
+export const SITE_TITLE = `李玉成`;
+export const SITE_DESCRIPTION = '博客站';
 export const SITE_QQ = 1911428610
 export const SITE_EMAIL = 'mungbeanlee@163.com'
 export const SITE_NAME = 'liyucheng.xyz';
