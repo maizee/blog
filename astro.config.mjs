07d32da4b78ce80a7badb977ef7f0bc9d2845e84
yuch <56346057+maizee@users.noreply.github.com>
2024-04-15 23:20:43 +0800

    增加文章

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
-import { defineConfig } from 'astro/config';
-import { visit } from 'unist-util-visit'
-import md5 from 'md5';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import { visit } from "unist-util-visit";
+import md5 from "md5";
+import sitemap from "@astrojs/sitemap";
 
-import { SITE_URL } from './src/consts';
-
+import { SITE_URL } from "./src/consts";
 
 function pipeline() {
   return [
-
     () => (tree) => {
-      visit(tree, 'element', (node, index) => {
-        if (node.tagName === 'p' && node.children[0].tagName === 'img') {
-          node.tagName = 'figure';
-          
+      visit(tree, "element", (node, index) => {
+        if (node.tagName === "p" && node.children[0].tagName === "img") {
+          node.tagName = "figure";
+
           let img = node.children[0];
           let sign = md5(img.properties.src);
           let data = img.properties.alt.split("|");
@@ -23,90 +21,87 @@ function pipeline() {
             size = data[1];
           }
 
-          let classes = ['image component image-fullbleed body-copy-wide nr-scroll-animation nr-scroll-animation--on'];
+          let classes = ["image component image-fullbleed body-copy-wide nr-scroll-animation nr-scroll-animation--on"];
           classes.push(`image-${size}`);
 
           node.properties.className = classes;
           node.children = [
             {
-              type: 'element',
-              tagName: 'div',
-              properties: { className: ['component-content'] },
+              type: "element",
+              tagName: "div",
+              properties: { className: ["component-content"] },
               children: [
                 {
-                  type: 'element',
-                  tagName: 'div',
-                  properties: { className: ['image-sharesheet'] },
+                  type: "element",
+                  tagName: "div",
+                  properties: { className: ["image-sharesheet"] },
                   children: [
                     {
-                      type: 'element',
-                      tagName: 'div',
+                      type: "element",
+                      tagName: "div",
                       properties: { className: [`image image-load image-asset image-${sign}`], id: `lht${sign}` },
                       children: [
                         {
-                          type: 'element',
-                          tagName: 'picture',
-                          properties: { className: ['picture'] },
+                          type: "element",
+                          tagName: "picture",
+                          properties: { className: ["picture"] },
                           children: [
                             {
-                              type: 'element',
-                              tagName: 'img',
+                              type: "element",
+                              tagName: "img",
                               properties: {
-                                'data-src': img.properties.src,
+                                "data-src": img.properties.src,
                                 alt: alt,
-                                className: ['picture-image'],
-                              }
-                            }
-                          ]
-                        }
-                      ]
-                    }
-                  ]
+                                className: ["picture-image"],
+                              },
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
                 },
                 {
-                  type: 'element',
-                  tagName: 'div',
-                  properties: { className: ['image-description'] },
+                  type: "element",
+                  tagName: "div",
+                  properties: { className: ["image-description"] },
                   children: [
                     {
-                      type: 'element',
-                      tagName: 'div',
-                      properties: { className: ['image-caption'] },
+                      type: "element",
+                      tagName: "div",
+                      properties: { className: ["image-caption"] },
                       children: [
                         {
-                          type: 'text',
-                          value: alt
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
+                          type: "text",
+                          value: alt,
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ];
         }
-      })
+      });
     },
 
     () => (tree) => {
       tree.children.forEach((node) => {
         if (node.type === "raw") {
-          node.value = `<div class="pagebody code component"><div class="component-content code"> ${node.value} </div></div>`
+          node.value = `<div class="pagebody code component"><div class="component-content code"> ${node.value} </div></div>`;
           // node.value = node.value.replace(/astro-code/g, 'astro-code')
         }
       });
     },
 
-
     () => (tree) => {
       for (let i = 0; i < tree.children.length; i++) {
         let node = tree.children[i];
-        if (node.type === "element" && ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', "ul", "ol","blockquote"].includes(node.tagName)) {
-
+        if (node.type === "element" && ["p", "h1", "h2", "h3", "h4", "h5", "h6", "ul", "ol", "blockquote"].includes(node.tagName)) {
           let next = tree.children[i + 1];
           let nodes = [node];
-          while (next && !['figure'].includes(next.tagName) && next.type != "raw") {
-
+          while (next && !["figure"].includes(next.tagName) && next.type != "raw") {
             nodes.push(next);
             next = tree.children[tree.children.indexOf(next) + 1];
           }
@@ -115,28 +110,27 @@ function pipeline() {
             // rename label
             nodes.forEach((node) => {
               if (node.tagName === "p") {
-                node.properties.className = ['pagebody-copy'];
+                node.properties.className = ["pagebody-copy"];
                 node.tagName = "div";
               }
-              if (['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(node.tagName)) {
-                node.properties.className = ['pagebody-header'];
+              if (["h1", "h2", "h3", "h4", "h5", "h6"].includes(node.tagName)) {
+                node.properties.className = ["pagebody-header"];
               }
             });
 
             tree.children.splice(i, nodes.length, {
-              type: 'element',
-              tagName: 'div',
-              properties: { className: ['pagebody  text component'] },
+              type: "element",
+              tagName: "div",
+              properties: { className: ["pagebody  text component"] },
               children: [
                 {
-                  type: 'element',
-                  tagName: 'div',
-                  properties: { className: ['component-content'] },
-                  children: nodes
-                }
-              ]
+                  type: "element",
+                  tagName: "div",
+                  properties: { className: ["component-content"] },
+                  children: nodes,
+                },
+              ],
             });
-
           }
         }
       }
@@ -147,30 +141,30 @@ function pipeline() {
         let node = tree.children[index];
         if (node.type === "element" && node.tagName === "figure") {
           tree.children.splice(index, 0, {
-            type: 'element',
-            tagName: 'div',
-            properties: { className: ['tertiarynav component'] },
-            children: [{
-              type: 'element',
-              tagName: 'div',
-              properties: { className: ['component-content'] },
-            }]
-          })
+            type: "element",
+            tagName: "div",
+            properties: { className: ["tertiarynav component"] },
+            children: [
+              {
+                type: "element",
+                tagName: "div",
+                properties: { className: ["component-content"] },
+              },
+            ],
+          });
           index++;
         }
       }
-    }
-  ]
+    },
+  ];
 }
-
-
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
   markdown: {
     rehypePlugins: pipeline(),
-    syntaxHighlight: 'prism',
+    syntaxHighlight: "prism",
   },
   integrations: [sitemap()],
 });
---
+++ src/pages/posts/weekly-4.md
@@ -0,0 +1,24 @@
+---
+layout: "../../layouts/MarkdownPost.astro"
+title: "周记第4期：你想活出怎样的人生"
+pubDate: 2024-04-14
+description: ""
+author: "李玉成"
+cover:
+  url: "https://img.3dmgame.com/uploads/images/news/20240314/1710382257_374600.jpg"
+  square: "https://img.3dmgame.com/uploads/images/news/20240314/1710382257_374600.jpg"
+  alt: "cover"
+tags: ["周刊"]
+theme: "light"
+featured: true
+---
+
+## 封面
+
+![|inline](https://img.3dmgame.com/uploads/images/news/20240314/1710382257_374600.jpg)
+
+宫崎骏导演的《你想活出怎样的人生》近期在大陆上映，我昨天晚上在电影院观看。天马行空的想象力，还是非常的好看。
+
+## 致两千年后的你
+
+![|inline](https://p.sda1.dev/16/69b11c3166a56a7ed9f605dd65b08ea0/CMP_20240412054652491.jpg)
